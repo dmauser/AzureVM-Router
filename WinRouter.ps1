65a7434109f153cd42a557bb9cf21b28103cb3eb
Daniel Mauser <dmauser@hotmail.com>
2020-08-04 14:40:21 -0500

    Update WinRouter.ps1

--- WinRouter.ps1
+++ WinRouter.ps1
@@ -1,6 +1,6 @@
 #Enable IP Forwarding in all interfaces
 Set-NetIPInterface -Forwarding Enabled
 
-#Enable ICMPv4 and ICMPv6
+#Enable ICMPv4 and ICMPv6 to Router for monitoring (Windows Firewall is enable by default and blocks ICMP Echo Requests)
 Set-NetfirewallRule -Name FPS-ICMP4-ERQ-In -Enable True -Profile Any
 Set-NetfirewallRule -Name FPS-ICMP6-ERQ-In -Enable True -Profile Any
