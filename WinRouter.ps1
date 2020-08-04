83368a2571f724a269beae2af8125095b282ab52
Daniel Mauser <dmauser@hotmail.com>
2020-08-04 14:22:24 -0500

    Updates

--- WinRouter.ps1
+++ WinRouter.ps1
@@ -1,1 +1,6 @@
+#Enable IP Forwarding in all interfaces
 Set-NetIPInterface -Forwarding Enabled
+
+#Enable ICMPv4 and ICMPv6
+Set-NetfirewallRule -Name FPS-ICMP4-ERQ-In -Enable True -Profile Any
+Set-NetfirewallRule -Name FPS-ICMP6-ERQ-In -Enable True -Profile Any
