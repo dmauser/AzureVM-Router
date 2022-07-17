4aa1e478cdc02e67ac8343bf6fe7fda768c507ff
Daniel Mauser <dmauser@hotmail.com>
2022-07-17 17:43:56 -0500

    Create linuxrouteronly.sh

---
+++ linuxrouteronly.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Enable IPv4 and IPv6 forwarding
+sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv6.conf.all.forwarding=1
+sed -i "/net.ipv4.ip_forward=1/ s/# *//" /etc/sysctl.conf
+sed -i "/net.ipv6.conf.all.forwarding=1/ s/# *//" /etc/sysctl.conf
