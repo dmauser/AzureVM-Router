232135ca4c9d5aaa48191f8927ca74afa980a6e7
Daniel Mauser <dmauser@hotmail.com>
2022-07-29 14:41:59 -0500

    update

--- linuxrouter.sh
+++ linuxrouter.sh
@@ -5,8 +5,16 @@ sysctl -w net.ipv6.conf.all.forwarding=1
 sed -i "/net.ipv4.ip_forward=1/ s/# *//" /etc/sysctl.conf
 sed -i "/net.ipv6.conf.all.forwarding=1/ s/# *//" /etc/sysctl.conf
 
+echo "Installing IPTables-Persistent"
+echo iptables-persistent iptables-persistent/autosave_v4 boolean false | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean false | sudo debconf-set-selections
+apt-get -y install iptables-persistent
+
 # Enable NAT to Internet
 iptables -t nat -A POSTROUTING -d 10.0.0.0/8 -j ACCEPT
 iptables -t nat -A POSTROUTING -d 172.16.0.0/12 -j ACCEPT
 iptables -t nat -A POSTROUTING -d 192.168.0.0/16 -j ACCEPT
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+# Save to IPTables file for persistence on reboot
+iptables-save > /etc/iptables/rules.v4
